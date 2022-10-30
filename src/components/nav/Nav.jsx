58fb358076b40c891eb7b4b8f4b66fa37abe89e4
Grinchenko Sergei <butters.blaga@gmail.com>
2022-10-30 23:47:07 +0900

    feat: Nav component completed

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import './nav.css';
+import { AiFillHome, AiOutlineUser } from 'react-icons/ai';
+import { BiBook, BiMessageSquareDetail } from 'react-icons/bi';
+import { RiServiceLine } from 'react-icons/ri';
 
 function Nav() {
+    const[activeNav, setActiveNav] = useState('#');
+
     return (
-        <div>Nav</div>
+        <nav>
+            <a href="#" className={activeNav === '#' ? 'active' : ''} onClick={() => setActiveNav('#')}><AiFillHome /></a>
+            <a href="#about" className={activeNav === '#about' ? 'active' : ''} onClick={() => setActiveNav('#about')}><AiOutlineUser /></a>
+            <a href="#experience" className={activeNav === '#experience' ? 'active' : ''} onClick={() => setActiveNav('#experience')}><BiBook /></a>
+            <a href="#services" className={activeNav === '#services' ? 'active' : ''} onClick={() => setActiveNav('#services')}><RiServiceLine /></a>
+            <a href="#contact" className={activeNav === '#contact' ? 'active' : ''} onClick={() => setActiveNav('#contact')}><BiMessageSquareDetail /></a>
+        </nav>
     )
 }
 
