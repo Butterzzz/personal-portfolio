29a4df4ba5469fe0aaf9e2ec63d855346678c6e1
Grinchenko Sergei <butters.blaga@gmail.com>
2023-03-29 01:09:18 +0900

    feat: update info about me

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -7,7 +7,7 @@ function Data() {
         <div className="home__data">
             <h1 className="home__title">Сергей Гринченко</h1>
             <h3 className="home__subtitle">Frontend разработчик</h3>
-            <p className="home__description">Я веб-разработчик из Благовещенска, и я очень увлечен и предан своей работе.</p>
+            <p className="home__description">👋 Я начинающий веб-разработчик из Благовещенска</p>
             <a href={CV} download className="button button--flex">
                 Скачать CV <CgFileDocument className="button__icon" />
             </a>
